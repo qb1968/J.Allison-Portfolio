20281bd08d090703c56020940f8adf2e32d7eeb8
qb1968 <qb1968@aol.com>
2020-06-09 15:02:56 -0400

    First

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,9 +9,9 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Hello, my name is',
+  name: 'Jonathan Allison',
+  subtitle: 'I am a Full Stack Developer',
   cta: '',
 };
 
@@ -21,37 +21,55 @@ export const aboutData = {
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
+    img: 'RM.png',
+    title: 'Rick and Morty',
     info: '',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://serene-tereshkova-2232d4.netlify.app',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
+    img: 'Wunder.png',
+    title: 'Wunderlist',
     info: '',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://frontend-f31fx9705.now.sh/',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
+    img: 'Game.png',
+    title: 'Game of Life',
     info: '',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://csbw1.now.sh',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'DDD.png',
+    title: 'SFTMA Data Analysis',
+    info: '',
+    info2: '',
+    url: 'https://datadriventransit.org',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'Movies.png',
+    title: 'Movie Database',
+    info: '',
+    info2: '',
+    url: 'https://movie-db-five.now.sh/',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
@@ -67,16 +85,6 @@ export const footerData = {
   networks: [
     {
       id: uuidv1(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: uuidv1(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: uuidv1(),
       name: 'linkedin',
       url: '',
     },
@@ -90,5 +98,5 @@ export const footerData = {
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };

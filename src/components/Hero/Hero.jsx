c9b5d86e1f1b61372dbaf1c49a201bcce2f36507
qb1968 <qb1968@aol.com>
2020-11-05 23:14:50 -0500

    whoops

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,39 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
-import styled, { keyframes } from 'styled-components';
-import zoomInUp from 'react-animations/lib/zoom-in-up';
-import zoomInDown from 'react-animations/lib/zoom-in-down';
+import Fade from 'react-reveal/Fade';
 import PortfolioContext from '../../context/context';
-
-const bounceAnimation = keyframes`${zoomInUp}`;
-const slideAnimation = keyframes`${zoomInDown}`;
-
-const SlideL = styled.h1`
-  animation: 8s ${slideAnimation};
-`;
-
-const ATag = styled.a`
-  animation: 8s ${bounceAnimation};
-`;
 
 const Header = () => {
   const { hero } = useContext(PortfolioContext);
   const { title, name, subtitle, cta } = hero;
 
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
   return (
     <section id="hero" className="jumbotron">
       <Container>
-        <SlideL className="hero-title">
-          {title || 'Hi, my name is'} <span className="text-color-main">{name || 'Your Name'}</span>
-          <br />
-          {subtitle || "I'm the Unknown Developer."}
-        </SlideL>
-
-        <p className="hero-cta">
-          <ATag className="cta-btn cta-btn--hero" href="#about">
-            {cta || 'Know more'}
-          </ATag>
-        </p>
+        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+          <h1 className="hero-title">
+            {title || 'Hi, my name is'}{' '}
+            <span className="text-color-main">{name || 'Your Name'}</span>
+            <br />
+            {subtitle || "I'm the Unknown Developer."}
+          </h1>
+        </Fade>
+        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+          <p className="hero-cta">
+            <a className="cta-btn cta-btn--hero" href="#about">
+              {cta || 'Know more'}
+            </a>
+          </p>
+        </Fade>
       </Container>
     </section>
   );
